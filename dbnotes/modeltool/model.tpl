4860812399884ba2bebad74f3ee77218a734e7f4
changjixiong <changjixiong@gmail.com>
2016-11-09 23:05:52 +0800

    generate model by template

--- dbnotes/modeltool/model.tpl
+++ dbnotes/modeltool/model.tpl
@@ -1,5 +1,6 @@
 package {{.PackageName}}
 
 type {{.ModelName | firstCharUpper}} struct {
-}
+{{range .TableSchema}} {{.COLUMN_NAME | exportColumn}} {{.DATA_TYPE | typeConvert}} {{.COLUMN_NAME | tags}} // {{.COLUMN_COMMENT}}
+{{end}}}
 
--- dbnotes/modeltool/modeltool.go
+++ dbnotes/modeltool/modeltool.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -19,10 +20,36 @@ SELECT
         FROM COLUMNS
         WHERE TABLE_NAME = 'mail'  and TABLE_SCHEMA = 'dbnote'
 */
+type ModelInfo struct {
+	PackageName string
+	ModelName   string
+	TableSchema *[]TABLE_SCHEMA
+}
 
-func genModelFile(tableName string, render *template.Template) {
-	os.Remove(tableName + ".go")
-	f, err := os.Create(tableName + ".go")
+type TABLE_SCHEMA struct {
+	COLUMN_NAME    string `db:"COLUMN_NAME" json:"column_name"`
+	DATA_TYPE      string `db:"DATA_TYPE" json:"data_type"`
+	COLUMN_COMMENT string `db:"COLUMN_COMMENT" json:"COLUMN_COMMENT"`
+}
+
+func genModelFile(render *template.Template, dbName, tableName string) {
+	tableSchema := &[]TABLE_SCHEMA{}
+	err := dbhelper.DB.Select(tableSchema,
+		"SELECT COLUMN_NAME, DATA_TYPE,COLUMN_COMMENT from COLUMNS where "+
+			"TABLE_NAME"+"='"+tableName+"' and "+"table_schema = '"+dbName+"'")
+	//fmt.Println(tableSchema)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//return
+
+	fileName := tableName + ".go"
+
+	os.Remove(fileName)
+	f, err := os.Create(fileName)
 
 	if err != nil {
 		fmt.Println(err)
@@ -30,10 +57,16 @@ func genModelFile(tableName string, render *template.Template) {
 	defer f.Close()
 
 	model := &ModelInfo{PackageName: "model",
-		ModelName: tableName}
+		ModelName:   tableName,
+		TableSchema: tableSchema}
+
 	if err := render.Execute(f, model); err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(fileName)
+	cmd := exec.Command("goimports", "-w", fileName)
+	//cmd := exec.Command("gofmt", "-w", fileName)
+	cmd.Run()
 }
 
 func firstCharUpper(str string) string {
@@ -44,16 +77,63 @@ func firstCharUpper(str string) string {
 	}
 }
 
-type ModelInfo struct {
-	PackageName string
-	ModelName   string
+func tags(columnName string) template.HTML {
+
+	return template.HTML("`db:" + `"` + columnName + `"` +
+		" json:" + `"` + columnName + "\"`")
+}
+
+func exportColumn(columnName string) string {
+	columnItems := strings.Split(columnName, "_")
+	columnItems[0] = firstCharUpper(columnItems[0])
+	for i := 0; i < len(columnItems); i++ {
+		if strings.ToUpper(columnItems[i]) == "ID" {
+			columnItems[i] = "ID"
+		}
+	}
+
+	return strings.Join(columnItems, "")
+
+}
+
+func typeConvert(str string) string {
+
+	switch str {
+	case "smallint", "tinyint":
+		return "int8"
+
+	case "varchar", "text", "longtext", "char":
+		return "string"
+
+	case "date":
+		return "string"
+
+	case "int":
+		return "int"
+
+	case "timestamp":
+		return "*time.Time"
+
+	case "bigint":
+		return "int64"
+
+	case "float", "double", "decimal":
+		return "float64"
+
+	default:
+		return str
+	}
 }
 
 func main() {
 
 	data, _ := ioutil.ReadFile("model.tpl")
 	render := template.Must(template.New("model").
-		Funcs(template.FuncMap{"firstCharUpper": firstCharUpper}).
+		Funcs(template.FuncMap{
+			"firstCharUpper": firstCharUpper,
+			"typeConvert":    typeConvert,
+			"tags":           tags,
+			"exportColumn":   exportColumn}).
 		Parse(string(data)))
 
 	dbName := "dbnote"
@@ -67,7 +147,9 @@ func main() {
 	}
 
 	for _, table := range tablaNames {
-		genModelFile(table, render)
+		genModelFile(render, dbName, table)
 	}
+
+	//genModelFile(render, "information_schema", "COLUMNS")
 
 }
