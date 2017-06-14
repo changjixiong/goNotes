b50eeccaa56507bcf81197f8f474099731672fb1
changjixiong <changjixiong@gmail.com>
2017-06-14 22:06:59 +0800

    add remark

--- nsqNotes/nsqConsumer/nsqConsumer.go
+++ nsqNotes/nsqConsumer/nsqConsumer.go
@@ -60,6 +60,11 @@ func MakeConsumer(topic, channel string, config *nsq.Config,
 	handle func(message *nsq.Message) error) {
 	consumer, _ := nsq.NewConsumer(topic, channel, config)
 	consumer.AddHandler(nsq.HandlerFunc(handle))
+
+	// 待深入了解
+	// 連線到 NSQ 叢集，而不是單個 NSQ，這樣更安全與可靠。
+	// err := q.ConnectToNSQLookupd("127.0.0.1:4161")
+
 	err := consumer.ConnectToNSQD("127.0.0.1:4150")
 	if err != nil {
 		log.Panic("Could not connect")
