e65880e0431ecd42fe0fe70ff4c6b1e624d24d72
changjixiong <changjixiong@gmail.com>
2018-01-03 21:23:45 +0800

    add filerename

--- ionotes/filerename/main.go
+++ ionotes/filerename/main.go
@@ -3,34 +3,52 @@ package main
 import (
 	"fmt"
 	"os"
+	"syscall"
 	"time"
 )
 
 func main() {
 
-	file, _ := os.OpenFile("rotate.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.FileMode(0644))
+	fileOldName := "rotate.log"
+	fileRename := "rotate1.log"
+	file, _ := os.OpenFile(fileOldName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.FileMode(0644))
 
+	fmt.Println("open file", fileOldName)
 	for i := 0; i < 5; i++ {
 		file.WriteString(fmt.Sprintf("%v line: %d\n", time.Now(), i))
-		time.Sleep(time.Second * 2)
 	}
 
-	err := os.Rename("rotate.log", "rotate1.log") //重命名 C:\\log\\2013.log 文件为install.txt
+	var statOldFile syscall.Stat_t
+	if err := syscall.Stat(fileOldName, &statOldFile); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(fileOldName, "statOldFile.Ino:", statOldFile.Ino)
+
+	err := os.Rename(fileOldName, fileRename)
 	if err != nil {
 		fmt.Println("file rename Error:", err)
 	}
+
+	fmt.Println("rename ", fileOldName, "->", fileRename)
+
+	var statRenamedFile syscall.Stat_t
+	if err := syscall.Stat(fileRename, &statRenamedFile); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("fileRename", "statRenamedFile.Ino:", statRenamedFile.Ino)
 
 	fmt.Println("file.Name:", file.Name())
 
 	for i := 5; i < 10; i++ {
 		file.WriteString(fmt.Sprintf("%v line: %d\n", time.Now(), i))
-		time.Sleep(time.Second * 2)
 	}
 
-	fileOld, err := os.OpenFile("rotate.log", os.O_WRONLY|os.O_APPEND, os.FileMode(0644))
+	fileOld, err := os.OpenFile(fileOldName, os.O_WRONLY|os.O_APPEND, os.FileMode(0644))
 
 	if nil != err {
-		fmt.Println("fileOld open error:", err)
+		fmt.Println(fileOldName, " open error:", err)
 	} else {
 		fmt.Println("fileOld.Name:", fileOld.Name())
 	}
